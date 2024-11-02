66d555d2eecdf2aa64191337c25646f7a96375f6
Deepak Sharma <deepssharma0312@gmail.com>
2024-11-02 02:32:00 +0000

    Initial Commit

---
+++ cypress/integration/examples/Test1.js
@@ -0,0 +1,16 @@
+// Spec files as we call as Test File
+
+describe('My First test suite',function() //Test suite name
+{
+
+
+    it('My first test case',function()  //Test case can be always written under "it" (description and function)
+{
+
+cy.visit("https://www.nirankari.org/")
+
+})
+
+}
+
+)
