145309dc11210d61f620dce9ef2ab4ebf9aa0b0e
Deepak Sharma <deepssharma0312@gmail.com>
2024-12-23 00:06:19 +0000

    ChindWindow2 Using Prop Function

---
+++ cypress/integration/examples/childWindow2.js
@@ -0,0 +1,17 @@
+/// <reference types = "Cypress" />
+
+// Picking the URL from DOM using Prop function of JQuery
+
+describe("First test suite for Child Window 2", function () {
+    it("First test case for Child Window 2", function () {
+        cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+        cy.get("#opentab").then(function (ele) {
+            const url = ele.prop("href")
+            cy.visit(url)
+            cy.origin(url, () => {
+                cy.get(".navbar-collapse a[href='about.html']").click()
+                cy.get(".page-banner-cont h2").should("contain","About Us")
+            })
+        })
+    })
+})
