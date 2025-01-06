bb8786196cc3ecb2422a2af5acd2058c3ca7900c
Deepak Sharma <deepssharma0312@gmail.com>
2025-01-06 00:22:15 +0000

    cypress mochawesome reporter and cloud report

--- cypress.config.js
+++ cypress.config.js
@@ -2,14 +2,19 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
 
+  video: true, //Video recoding will be a default now
   defaultCommandTimeout: 6000,
   env: {
-    url: "https://rahulshettyacademy.com"
+    url: "https://rahulshettyacademy.com/loginpagePractise/"
 
   },
+  projectId: "rosigg",  //Project id fpr the cloud report genration
+  retries: 1, //Reattempts the failes Spec
+  reporter: 'cypress-mochawesome-reporter',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on); //Created a mochawesome-reporter listener
     },
     specPattern: 'cypress/integration/examples/*.js'
   },
